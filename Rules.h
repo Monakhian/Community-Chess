c6e868394348c66f3e7bc0313bf9471c22801b8f
Monakhian <iandmonk@gmail.com>
2026-05-05 05:24:30 -0600

    Minor edits to Rules.h

--- Rules.h
+++ Rules.h
@@ -22,8 +22,8 @@ public:
 class Move {
 	// Functions below mimic having mutable member functions
 	// Allows for rules to modify the behavior of moves without needing to create new move types for every rule change
-	// Example 1: Mutating apply allows for pawn promotion
-	// Example 2: Mutating string_rep allows for castling to be denoted as O-O and O-O-O instead of Kg1 and Kc1
+	// Example 1: Mutating "apply" allows for pawn promotion
+	// Example 2: Mutating "string_rep" allows for castling to be denoted as O-O and O-O-O instead of Kg1 and Kc1
 	// Further, this allows for custom rules such as swap chess and duck chess to define their own notation
 	std::function<void(const Move&, ChessBoard&)> apply = [](const Move& move, ChessBoard& board) {  board.move_piece(move.fromFile, move.fromRank, move.toFile, move.toRank); };
 	std::function<std::string(const Move&, const ChessBoard&)> string_rep = [](const Move& move, const ChessBoard& board) {
@@ -53,23 +53,25 @@ public:
 
 class MoveGenerator : public Rule {
 public:
-	virtual std::vector<Move> get_moves(int file, int rank) {}
+	virtual std::vector<Move> get_moves(int file, int rank) = 0;
 };
 
 class MoveTransformer : public Rule {
 public:
-	virtual void apply(std::vector<Move>& moves) {}
+	virtual void apply(std::vector<Move>& moves) = 0;
 };
 
 class MoveRestrictor : public Rule {
 public:
-	virtual void apply(std::vector<Move>& moves) {}
+	virtual void apply(std::vector<Move>& moves) = 0;
 };
 
 #endif
 
 // Code to be executed when building as an executable for testing purposes
-#ifdef BUILD_AS_EXE
+// only works with gcc/clang compilers
+// notably does not work with MSVC (visual studio code)
+#if __INCLUDE_LEVEL__ == 0
 
 #include <iostream>
 
